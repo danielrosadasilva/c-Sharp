db27e8bb10069ce5419c3b0745ab11e97a72bdda
Daniel Rosa <105619659+danielrosadasilva@users.noreply.github.com>
2022-10-26 02:19:46 -0300

    estudos

--- ex01/aula.cs
+++ ex01/aula.cs
@@ -1,19 +1,35 @@
 using System;
-class principal{
-    
-    static void Main(string[] args){
-        string nome= "daniel";
-        char sexo='m';
-        int idade=10;
-        Console.Write("ola sou"+nome+"idade"+idade);
-        Console.Write("sexo"+sexo);
-        somanumero(10,15);
-        console.readline();
-        
-    }
+using Accessibility;
+using System.Collections.Generic;
 
-    static void somanumero(int num1,int num2){
-        int aux = num1 + num2;
-        Console.Writeline("resultado da soma de numero"+num1)+"numero"+num2+"é igual="+aux;
+internal class NewBaseType
+{
+    static void Main()
+    {
+       string nome = "daniel";
+        char sexo = 'm';
+        int idade = 10;
+        Console.Write("ola sou" + nome + "idade" + idade);
+        Console.Write("sexo" + sexo);
+        somaNumero(15,12);
+        List<string> a=new List<string>();
+        a.Add("daniel");
+        a.Add("julina");
+        a.Add("carlos");
+        a.Sort;
+        foreach (String nome in a){
+            Console.WriteLine(nome);
+        }
+        Console.WriteLine(a.Count);
+        Console.WriteLine("ola");
     }
+    static void somaNumero(int num1,int num2)
+        {
+            int aux =  num1 + num2;
+            Console.WriteLine(" resultado da soma de numero "+num1+" numero "+num2+" é igual= "+aux);
+        }
+   
+
 }
+
+    
