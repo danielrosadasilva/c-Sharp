0d34f2b77ee2386d4075cbc823553dfd6e16e895
Daniel Rosa <105619659+danielrosadasilva@users.noreply.github.com>
2022-10-27 01:07:57 -0300

    ex002

---
+++ ex02/ConsoleAppCurso/ConsoleApp1/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private string email;  
+        private string endereco;
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getNome()
+        {
+            return this.nome;   
+        }
+        public  string getEmail()
+        {
+            return this.email;
+        }
+        
+    }
+    
+}
---
+++ ex02/ConsoleAppCurso/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1;
+using System;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numeros = new int[5];
+        string[] names = new string[5];
+        for(int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine("Digite na posicao {0} o valor: ", i);
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+        foreach (int i in numeros)
+        {
+            Console.WriteLine(i);
+        }
+        for (int i=0; i < names.Length; i++)
+        {
+            Console.WriteLine("Digite um nome na posicao {0} :",i);
+            names[i]=Console.ReadLine();
+        }
+        foreach(string n in names) //foreach apenas para leitura de dados,não funciona pra gravar valores 
+        {
+            Console.WriteLine(n);
+        }
+        verificar(numeros);
+       
+     
+    }
+    public static void verificar(int []array)
+    {
+        if (array.Length == 5)
+        {
+            Console.WriteLine("está cheio");
+        }
+        else
+        {
+            Console.WriteLine("não esta cheio");
+        }
+    }
+    
+}
