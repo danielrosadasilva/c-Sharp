da616ce87d9a6853acfe9f5697dc02172fad4dc9
Daniel Rosa <105619659+danielrosadasilva@users.noreply.github.com>
2022-11-19 10:25:57 -0300

    consumir api

---
+++ consumirAPI/consumirAPI/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using QuickType;
+using System.Reflection.Metadata;
+using System.Text.Json.Serialization;
+
+//https://quicktype.io/csharp    site para converter json em c#
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+        var response=await client.GetAsync("https://jsonplaceholder.typicode.com/users");       //site de teste de API
+        var content=await response.Content.ReadAsStringAsync();
+        var users = JsonConvert.DeserializeObject<User[]>(content);
+
+        foreach(var item in users)
+        {
+            Console.WriteLine(item.Name);
+            Console.WriteLine(item.Email);
+            Console.WriteLine(item.Address);
+            Console.WriteLine(item.Company);
+            Console.WriteLine("---------------------");
+        }
+
+    }
+}
+
